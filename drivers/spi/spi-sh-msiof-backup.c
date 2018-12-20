dae1d965746f166d912e9609bdfbfe49de45259d
Hoan Nguyen An <na-hoan@jinso.co.jp>
2018-12-20 11:47:05 +0900

    msiof: Reduce the number of times write to FIFO and number of times perform the transmission from FIFO.

--- drivers/spi/spi-sh-msiof-backup.c
+++ drivers/spi/spi-sh-msiof-backup.c
@@ -1246,7 +1246,7 @@ static int sh_msiof_transfer_one(struct spi_master *master,
 
 	if(n_transfer_one >= 7)
 	{
-		unsigned int t_len = 67;
+		unsigned int t_len = 193;
 		u8* data_t = (u8 *)kmalloc(t_len * sizeof(u8*), GFP_KERNEL);
 		for (i = 0; i < t_len; i++)
 		{
@@ -1352,7 +1352,7 @@ static int sh_msiof_transfer_one(struct spi_master *master,
 //	}
 
 	printk("file %s func %s line %d if (bits <= 8 && %d > 3 && !(%d & 3) %d", __FILE__,__FUNCTION__, __LINE__, len, len, bits <= 8 && len > 3 && !(len & 3));
-	if (bits <= 8 && len > 15 && !(len & 3)) {
+	if (bits <= 8 && len > 3) {
 		bits = 32;
 		swab = true;
 		h_debug;
@@ -1401,14 +1401,19 @@ static int sh_msiof_transfer_one(struct spi_master *master,
 	}
 
 	/* transfer in fifo sized chunks */
-	words = len / bytes_per_word;
+	words = len / bytes_per_word + len % bytes_per_word;
 
 	printk("file %s func %s line %d bytes_per_word %d words %d", __FILE__, __FUNCTION__, __LINE__,bytes_per_word, words);
 
 	while (words > 0) {
+		if (words == len % bytes_per_word) {
+			bytes_per_word = 1;
+			tx_fifo = sh_msiof_spi_write_fifo_8;
+			rx_fifo = sh_msiof_spi_read_fifo_8;
+			bits = t->bits_per_word;
+		}
 		n = sh_msiof_spi_txrx_once(p, tx_fifo, rx_fifo, tx_buf, rx_buf,
 					   words, bits);
-		printk("file %s func %s line %d n_words %d", __FILE__, __FUNCTION__, __LINE__, n);
 		if (n < 0)
 			return n;
 
